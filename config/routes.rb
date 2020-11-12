a116f7edbb56296b29d649339b639f9eb70a37e3
Thomas Van Holder <thomasvanholder@hey.com>
2020-11-12 08:42:01 +0100

    command out pry

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root to: "restaurants#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "restaurants#index"
   resources :restaurants
   get '/restaurants/:id/qrcode', to: "restaurants#qrcode", as: "restaurant_qrcode"
   post "/send-email-qr-code", to: "restaurants#send_email_qr_code"
