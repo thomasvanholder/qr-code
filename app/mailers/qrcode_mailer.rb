136f285990511da5e3d0def6aad1deec6296f17b
Thomas Van Holder <thomasvanholder@hey.com>
2020-11-12 12:53:08 +0100

    update to actionmailer production display QR as SVG

--- app/mailers/qrcode_mailer.rb
+++ app/mailers/qrcode_mailer.rb
@@ -3,9 +3,11 @@ class QrcodeMailer < ApplicationMailer
   def send_qr_code_email
     @restaurant = params[:restaurant]
 
+
+
     link = "http://localhost3000/restaurants/" + @restaurant.id.to_s + "/qrcode?"
     instance = RQRCode::QRCode.new(link)
-    @svg = instance.as_svg(
+    attachments.inline["qr-code.svg"] = instance.as_svg(
       offset: 0, # no padding
       color: "000", # color black
       shape_rendering: "crispEdges",
