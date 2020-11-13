a567095d89d06ac616177da0e4ce9dc537971f78
Thomas Van Holder <thomasvanholder@hey.com>
2020-11-13 07:23:25 +0100

    pic p

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,8 @@ Rails.application.configure do
   config.assets.compile = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = 'http://assets.example.com'
+  config.action_controller.asset_host = 'https://www.qr-codemenu.com"'
+  config.action_mailer.asset_host = config.action_controller.asset_host
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
@@ -67,6 +68,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { host: "https://www.qr-codemenu.com"}
+
+
+
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
