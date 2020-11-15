9c844a5a52c58d847b7a43029d4cac835002a6aa
Thomas Van Holder <thomasvanholder@hey.com>
2020-11-15 09:01:33 +0100

    up

--- app/javascript/controllers/active-tab_controller.js
+++ app/javascript/controllers/active-tab_controller.js
@@ -1,14 +1,25 @@
 import { Controller } from "stimulus";
 
 export default class extends Controller {
-  static targets = ["nav", "section", "hide_phone"];
+  static targets = [
+    "nav",
+    "section",
+    "hide_phone",
+    "items_content"
+  ];
 
   connect() {
     // console.log("Hello, Stimulus!", this.element);
   }
 
   isInViewport(el) {
+    // set rect to iphone boudingRect
+    console.log("section rect")
     const rect = el.getBoundingClientRect();
+    console.log(rect)
+    console.log("phone_content rect")
+    const content = this.items_contentTarget.getBoundingClientRect()
+    console.log(content)
     return (
         rect.top >= 0 &&
         rect.left >= 0 &&
