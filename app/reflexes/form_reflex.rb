c1149c5a1801d510c4fb325992f5e4f61156e12d
Thomas Van Holder <thomasvanholder@hey.com>
2020-11-15 08:44:47 +0100

    edit and new form working

--- app/reflexes/form_reflex.rb
+++ app/reflexes/form_reflex.rb
@@ -9,21 +9,20 @@ class FormReflex < ApplicationReflex
     id = element.dataset.category_id || extract_id(element.name)
     morph "#category-#{id}", element.value
     morph "#tab-#{id}", element.value
-    # byebug
   end
 
   def print_item
-    id = element.dataset.item
+    id = element.dataset.item || extract_menu_id(element.name)
     morph "#item-name-#{id}", element.value
   end
 
   def print_price
-    id = element.dataset.item
+    id = element.dataset.item || extract_menu_id(element.name)
     morph "#item-price-#{id}", "$#{element.value}"
   end
 
   def print_description
-    id = element.dataset.item
+    id = element.dataset.item || extract_menu_id(element.name)
     morph "#item-description-#{id}", element.value
   end
 
@@ -33,5 +32,14 @@ class FormReflex < ApplicationReflex
     # get 5 or more digits to match [1604647606030]
     string.match(/\d{1,}/)[0]
   end
+
+
+
+  def extract_menu_id(string)
+    sol = string.match(/items_attributes\S\S\d{1,}/)[0]
+    sol.scan(/\d+/).first
+  end
 end
+
+
 
