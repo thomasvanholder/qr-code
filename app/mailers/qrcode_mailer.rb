1bd76df03253044f7a84e943c33eb58f4efff510
Thomas Van Holder <thomasvanholder@hey.com>
2020-11-15 13:18:05 +0100

    chatbot config

--- app/mailers/qrcode_mailer.rb
+++ app/mailers/qrcode_mailer.rb
@@ -2,7 +2,7 @@ class QrcodeMailer < ApplicationMailer
 
   def send_qr_code_email
     @restaurant = params[:restaurant]
-    attachments.inline["qr-code-example.png"]  = File.read("#{Rails.root}/app/assets/images/qr-code-example.png")
+    attachments.inline["qr-example.png"] = File.read("#{Rails.root}/app/assets/images/qr-code-example.png")
 
     mail(
       to: @restaurant.email,
